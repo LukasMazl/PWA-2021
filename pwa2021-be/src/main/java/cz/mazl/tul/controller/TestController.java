cb9038dbfb253e61ede37b0e529597d42fcb64d1
Lukas Mazl <lukas.mazl@plus4u.net>
2020-10-18 09:33:40 +0200

    Rest controller test

--- pwa2021-be/src/main/java/cz/mazl/tul/controller/TestController.java
+++ pwa2021-be/src/main/java/cz/mazl/tul/controller/TestController.java
@@ -1,10 +1,10 @@
 package cz.mazl.tul.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @RequestMapping(path = "/test", method = RequestMethod.GET)
