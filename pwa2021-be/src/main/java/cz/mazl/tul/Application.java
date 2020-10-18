eed4d1a26b0ded07fbf99b0e7f61b43f3dd3d9d0
Lukas Mazl <lukas.mazl@plus4u.net>
2020-10-18 09:28:02 +0200

    Repair

--- pwa2021-be/src/main/java/cz/mazl/tul/Application.java
+++ pwa2021-be/src/main/java/cz/mazl/tul/Application.java
@@ -2,11 +2,12 @@ package cz.mazl.tul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
